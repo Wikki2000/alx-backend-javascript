5d2d02f72aa63aa20afb695d419242d938ba854d
wikki2000 <wisdomokposin@gmail.com>
2024-10-04 14:57:14 +0100

    Add: 3-currency.js

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,26 @@
+export default class Currency {
+  constructor(code, name) {
+    this.name = name;
+    this.code = code;
+  }
+
+  /* ==== Getter Method of Attributes === */
+  get name() {
+    return this._name;
+  }
+  get code() {
+    return this._code;
+  }
+
+  /* ==== Setter Method of Attributes === */
+  set name(val) {
+    this._name = val;
+  }
+  set code(val) {
+    this._code = val;
+  }
+
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
+}
